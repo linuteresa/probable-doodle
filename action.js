42b3bdcb4525d77e362cd85bab2c36c9d0c662da
Linu Anil Teresa <78141612+linuteresa@users.noreply.github.com>
2021-05-23 10:22:43 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,12 @@
+function  yellow()
+{
+document.body.style.background="yellow"
+}
+function white()
+{
+    document.body.style.background="white"
+}
+function red()
+{
+    document.body.style.background="red"
+}
